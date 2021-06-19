16037641fa9f70509ecdf6b4a90beead19ee2a14
sanchibud <sanchibud@gmail.com>
2021-06-19 22:40:39 +0100

    java code

---
+++ src/main/java/com/sanchibud/App.java
@@ -0,0 +1,8 @@
+package com.sanchibud;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World. From sanchibud");
+    }
+}
---
+++ src/test/java/com/sanchibud/AppTest.java
@@ -0,0 +1,18 @@
+package com.sanchibud;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Comment
+     */
+    @Test
+    public void ReturnTrue()
+    {
+        assertTrue( true );
+    }
+}
